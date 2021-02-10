a5e7797c3ef7b9c1acc0a92e9646ef9cd09ad5f2
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-09 19:03:54 -0500

    commit tree b694f662ac527583b447cd6f6002b4b021fe0948

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 // Your code here
+let arr = ["firstName", "familyName", "title", "payPerHour"]
+
+
+
+function createEmployeeRecord(e){
+    return {
+        firstName: e[0],
+        familyName: e[1],
+        title: e[2],
+        payPerHour: e[3],
+        timeInEvents: [],
+        timeOutEvents: []
+        }
+        //find firstName from array
+    //this.timeInEvents = timeInEvents
+    //this.timeOutEvents =  timeOutEvents
+}
+
+const arr1 = {"firstName": "Loki" , "familyName": "sizlak", "title": "barkeep", payPerHour: 10}
+const arr2 = {"firstName": "Natalia" , "familyName": "simpson", "title": "scamp", payPerHour: 3}
+
+
+function createEmployeeRecords(){
+    let length = []
+
+    length.push(arr1, arr2)
+    return length
+   // createEmployeeRecord.append = all
+   //adding each return value of createEmployeeRecord to this function
+
+
+}
+
+//let dateStamp = 'YYYY-MM-DD HHMM'
+// function createTimeInEvent(obj, date){
+
+// }
+
+// function createTimeOutEvent(){
+
+// }
+
+// function hoursWorkedOnDate(){
+
+// }
+
+function wagesEarnedOnDate(){
+
+}
+
+function allWagesFor(element, index, array){
+    return(element === 54)
+//find employees that earned $54
+}
+
+function findEmployeeByFirstName(){
+
+}
+
+function calculatePayroll(){
+
+}
+
+
+function createTimeInEvent(employee, dateStamp){ /* */ }
+function createTimeOutEvent(employee, dateStamp){ /* */ }
+function hoursWorkedOnDate(employee, soughtDate){ /* */ } 
